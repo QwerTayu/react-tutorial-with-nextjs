07c077b936f14c8e3196acefb22a3387473991f4
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-13 17:14:28 +0900

    ✨ useStateで文字列と真偽値について理解しよう

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,32 +5,56 @@ import { useCallback, useEffect, useState } from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [inputAlert, setInputAlert] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count]);
-
   
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if(e.target.value.length > 5){
+      setInputAlert("5文字以内にしてね");
+      return;
+    };
+    setInputAlert("");
+    setText(e.target.value.trim());
+  }, []);
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     };
   }, []);
+  console.log(text);
 
   return (
     <>
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button 
         onClick={(e) => handleClick(e)}
         // onClick={(e) => {handleClick(e), foo}}
       >
         ボタン
       </button>
+      <button onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <input 
+        type="text"
+        value={text}
+        onChange={handleChange}
+      />
+      <p>{inputAlert}</p>
       <Main page="index" />
     </>
   )
