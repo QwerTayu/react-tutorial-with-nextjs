602414422b336b77b3f49ff084dfc6dcfa9416e0
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-13 11:35:22 +0900

    edit: コンポーネント化したlinksのリンクを修正

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Header } from '@/components/header'
-import { Links } from '@/components/Links'
+import { Links } from '@/components/links'
 
 const inter = Inter({ subsets: ['latin'] })
 
