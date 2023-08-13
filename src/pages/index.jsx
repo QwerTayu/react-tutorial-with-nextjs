fd249b1b8fe3313edf255333a358c16765d167b8
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-13 16:07:22 +0900

    ✨ useEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,39 @@
 import { Inter } from 'next/font/google'
 import Main from '@/src/components/main/Main'
+import { useCallback, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 
-const handleClick = (e, foo) => {
-  e.preventDefault();
-  alert(foo);
-}
-
+// const handleClick = (e, foo) => {
+//   e.preventDefault();
+//   alert(foo);
+// }
 export default function Home() {
 
   const foo=1;
+
+  const handleClick = useCallback((e) => {
+    e.preventDefault();
+    alert(foo);
+  }, [])
+
+  
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    console.log("mounted");
+    return () => {
+      document.body.style.backgroundColor = "";
+      console.log("unmounted");
+    };
+  }, []);
 
   return (
     <>
       <a 
         href="/about"
-        onClick={(e) => {
-          handleClick(e, foo)
-        }}
+        onClick={(e) => handleClick(e)}
+        // onClick={(e) => {handleClick(e), foo}}
       >
         ボタン
       </a>
