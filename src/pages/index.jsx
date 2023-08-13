d0f81bfce0ec8e3af75896a709088d971754121b
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-13 16:45:24 +0900

    ⚡️ useCallBack, useEffectの第２引数を実装しました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,18 @@
 import { Inter } from 'next/font/google'
 import Main from '@/src/components/main/Main'
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
-
-// const handleClick = (e, foo) => {
-//   e.preventDefault();
-//   alert(foo);
-// }
 export default function Home() {
 
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   
   useEffect(() => {
